371ced3ba6f6aa78222a7b42a43ca16686b4f4cd
11th WHS Trenton J Greenlee <061935@SWCS.K12>
2020-01-23 13:39:39 -0500

    Initial commit

---
+++ src/com/company/Greenlee/Main.java
@@ -0,0 +1,42 @@
+package com.company.Greenlee;
+import java.util.Scanner;
+import java.util.*;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int age = 17;
+        double GPA = 4.0;
+        boolean isRaining = false;
+        String city = "Columbus";
+        System.out.println(city);
+        System.out.println(city.toUpperCase());
+        int dailyHighs[] = {50, 60, 70, 80, 90};
+        /*
+        System.out.println(dailyHighs[0]);
+        System.out.println(dailyHighs[1]);
+        System.out.println(dailyHighs[2]);
+        System.out.println(dailyHighs[3]);
+        System.out.println(dailyHighs[4]);
+        */
+        for(int i = 0; i <= 4; i++) {
+            System.out.println(dailyHighs[i]);
+        }
+
+        int twoDArray[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        System.out.println(twoDArray[0][1]);
+        System.out.println(twoDArray[2][3]);
+
+        int sum = 2 + 3;
+        System.out.println(sum);
+
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
